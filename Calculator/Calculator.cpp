a12bd605230dc3394e712f91a82a7cec6b8d76c8
Igor Tokmakov <idtokmakov@edu.hse.ru>
2023-12-18 19:21:00 +0500

    nums and letters

--- Calculator/Calculator.cpp
+++ Calculator/Calculator.cpp
@@ -1,5 +1,7 @@
-﻿#include <iostream>
+﻿#include <Windows.h>
+#include <iostream>
 #include <string>
+#include <thread>
 using namespace std;
 
 struct Tree {
@@ -8,11 +10,21 @@ struct Tree {
     Tree* right = nullptr;
 };
 
+struct List {
+    char letter;
+    int value;
+    List* next = nullptr;
+};
+
 static string ExprStr;
 static char scanSymbol;
 static int SI;
 static string DIGIT = "0123456789";
+static string LETTER = "abcdefghijklmnopqrstuvwxyz";
 Tree* root;
+List* letters = nullptr;
+
+
 
 static void InsNode(Tree*& tree, char c) {
     tree = new Tree();
@@ -44,6 +56,15 @@ static bool Contain(string syms, char sym) {
     return false;
 }
 
+static void Number(Tree* tree) {
+    tree->inf = scanSymbol;
+    NextSymbol();
+    if (Contain(DIGIT, scanSymbol) && SI != -1) {
+        tree->right = new Tree();
+        Number(tree->right);
+    }
+}
+
 static void ADDEND(Tree*& tree);
 
 static void EXPRESSION(Tree*& tree) {
@@ -65,12 +86,41 @@ static void EXPRESSION(Tree*& tree) {
 
     }
     else {
-        cout << "Wrong beginning\n";
+        cout << "Неверное начало выражения - " << SI << "\n";
     }
 }
 
+static void Factor(Tree*& tree);
+
 static void ADDEND(Tree*& tree) {
+    Tree* O = nullptr;
+    if (Contain(DIGIT, scanSymbol) || scanSymbol == '(' || Contain(LETTER, scanSymbol)) {
+        Factor(O);
+        while (((scanSymbol == '*') || (scanSymbol == '/')) && (SI != -1)) {
+            InsNode(tree, scanSymbol);
+            tree->left = O;
+            NextSymbol();
+            if (SI != -1) {
+                Factor(tree->right);
+            }
+            O = tree;
+        }
+        if (tree == nullptr)
+            tree = O;
+
+    }
+    else {
+
+        cout << "Неверное начало выражения - " << SI << "\n";
+    }
+}
+
+static void Factor(Tree*& tree) {
     if (Contain(DIGIT, scanSymbol)) {
+        tree = new Tree();
+        Number(tree);
+    }
+    else if (Contain(LETTER, scanSymbol)) {
         InsNode(tree, scanSymbol);
         NextSymbol();
     }
@@ -82,44 +132,88 @@ static void ADDEND(Tree*& tree) {
             if (SI != -1 && scanSymbol == ')')
                 NextSymbol();
             else
-                cout << "There is no last brake\n";
+                cout << "Нет завершающей скобки\n";
         }
         else {
-            cout << "Wrong beginning\n";
+            cout << "Неверное начало выражения - " << SI << "\n";
         }
+    }
+}
+
+static int TakeNumber(Tree* tree, int num = 0) {
+    if (tree->right != nullptr) {
+        return TakeNumber(tree->right, ((int)tree->inf - 48 + num) * 10);
+    }
+    return num + (int)tree->inf - 48;
+}
+
+static int TakeLetter() {
+    List* list = letters;
+    while (list != nullptr && list->letter != scanSymbol) {
+        list = list->next;
+    }
+    if (list != nullptr)
+        return list->value;
+    else {
+        cout << "Введите значение переменной " << scanSymbol << ": ";
+        int result;
+        cin >> result;
+        list = new List();
+        list->letter = scanSymbol;
+        list->value = result;
+        if (letters == nullptr) {
+            letters = list;
+        }
+        else {
+            List* temp = letters;
+            while (temp->next != nullptr) {
+                temp = temp->next;
+            }
+            temp->next = list;
+        }
+        return result;
     }
 }
 
 static int Calculate(Tree*& tree) {
     if (Contain(DIGIT, tree->inf))
-        return (int)tree->inf - 48;
+        return TakeNumber(tree);
+    else if (Contain(LETTER, tree->inf))
+        return TakeLetter();
     else {
+        int res1 = Calculate(tree->left);
+        int res2 = Calculate(tree->right);
         if (tree->inf == '+')
-            return Calculate(tree->left) + Calculate(tree->right);
-        else
-            return Calculate(tree->left) - Calculate(tree->right);
+            return res1 + res2;
+        else if (tree->inf == '-' )
+            return res1 - res2;
+        else if (tree->inf == '*')
+            return res1 * res2;
+        else if(tree->inf == '/')
+            return res1 / res2;
     }
 
 }
 
-
 int main()
 {
+    setlocale(LC_ALL, "Russian");
+
     root = nullptr;
-    cout << "Write an expression:\n";
+    cout << "Введите выражение:\n";
     cin >> ExprStr;
     SI = 0;
     scanSymbol = ExprStr[SI];
     EXPRESSION(root);
     if (SI == -1){
-        cout << "Correct\n";
+        cout << "Выражение составлено правильно\n";
     }
     else {
-        cout << "Useless symbols\n";
+        cout << "Присутствуют лишние символы\n";
     }
     if (root != nullptr) {
         cout << "tree:\n";
         WriteExpr(root);
-        cout << endl << "The result = " << Calculate(root) << endl;
+        cout << endl << "Результат = " << Calculate(root) << endl;
     }
 }
